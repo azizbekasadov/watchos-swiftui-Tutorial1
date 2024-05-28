59de998946456fb3fe070cfb48f6ca7aa1995484
Azizbek Asadov <azizbek.asadov@vilea.ch>
2024-05-29 00:44:56 +0200

    proj + anims

--- Tutorial1 Watch App/ContentView.swift
+++ Tutorial1 Watch App/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moveOnPath = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Simple Animation")
+                .font(.system(size: 18, weight: .semibold))
+                .foregroundStyle(Color.white)
+            
+            ZStack {
+                Circle()
+                    .stroke()
+                    .frame(width: 100, height: 100, alignment: .center)
+                
+                Circle()
+                    .stroke()
+                    .frame(width: 15, height: 15, alignment: .center)
+                    .foregroundStyle(Color.red)
+                    .offset(x: -50)
+                    .rotationEffect(.degrees(moveOnPath ? 0 : 360))
+                    .animation(
+                        Animation.linear(duration: 4)
+                            .repeatForever(autoreverses: false)
+                    )
+                    .onAppear(perform: {
+                        moveOnPath.toggle()
+                    })
+                
+            }
+            .rotation3DEffect(
+                .degrees(70),
+                axis: (x: 10, y: 0.5, z: 0.0)
+            )
+            
+            Text("WatchOS Tutorials")
+                .font(.caption2)
+                .foregroundStyle(Color.white)
         }
-        .padding()
     }
 }
 
--- Tutorial1/ContentView.swift
+++ Tutorial1/ContentView.swift
@@ -8,14 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moveOnPath = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
         }
-        .padding()
     }
 }
 
